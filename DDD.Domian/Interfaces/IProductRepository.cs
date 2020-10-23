834a0ed491d86a36375546fd86db66fbdcbe2a59
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2020-10-23 09:42:01 -0300

    Delete IProductRepository.cs

--- DDD.Domian/Interfaces/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using DDD.Domian.Entities;
-using System.Collections.Generic;
-namespace DDD.Domian.Interfaces
-{
-    public interface IProductRepository : IRepositoryBase<Product>
-    {
-        IEnumerable<Product> SearchByName(string name);
-    }
-}
